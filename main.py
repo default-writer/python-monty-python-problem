7abe1c584ab746065023ee17cc0a433ec03c9bd0
Artur Mustafin <artur.mustafin@gmail.com>
2020-11-14 22:06:55 +0000

    added documentation

--- main.py
+++ main.py
@@ -1,26 +1,40 @@
 
 from random import shuffle, choice
-
 #
-N = 5
+# Monty's chances improvements are
+#
+# imrovement is 1/n*(n-2) so chances are 1/n + 1/n(n-2) = (n-1)/n(n-2)
+#
+N = 9
+# at 10 doors it is 1/n*(n-2) or 0.0125 chances improvement
+# at 9 it is 0.(015873) chances improvement
+# at 8 it is 0.0208(3) chances improvement
+# at 7 it is 0.0(285714) chances improvement
+# at 6 it is 0.041(6) chances improvement
+# at 5 it is 0.0(6) chances improvement
+# at 4 it is 0.125 chances improvement
+# at 3 it is 0.(3) chances improvement
 
+# door generator
 def Doors(n):
-   i = n - 1
-   yield 1
-   while i > 0:
-     yield 0
-     i = i - 1
+  i = n - 1
+  yield 1
+  while i > 0:
+    yield 0
+    i = i - 1
 
 class Monty:
-    def __init__(self):
-        self.vars = list(Doors(N))
-        shuffle(self.vars)
+  def __init__(self):
+    """Create 1 in random position"""
+    self.vars = list(Doors(N))
+    shuffle(self.vars)
 
-    def open(self):
-        return choice([x for x in range(1, N) if not self.vars[x]])
+  def open(self):
+    """Select any other door, than chosen by default"""
+    return choice([x for x in range(1, N) if not self.vars[x]])
 
-    def check(self, var=0):
-        return self.vars[var]
+  def check(self, var=0):
+    return self.vars[var]
 
 n = 1000
 
@@ -40,8 +54,8 @@ for i in range(10):
       m = Monty()
       h = m.open()
       v = choice([x for x in range(1, N) if x != h])
-      #stat2 += m.check() # that will return the same success rates
-      stat2 += m.check(v) # that will return imporved success rates
+      #stat2 += m.check() # this will return the same success rates
+      stat2 += m.check(v) # this will return imporved success rates
   stat2 = stat2/n
   result2.append(stat2)
 
